9f51f926224e7cc696bccfdd2b6f6a5ea603e83d
Rishurp <rishupandey3102@gmail.com>
2025-02-21 23:50:37 +0530

    added styling and infinite scrolling logic

--- infinite-scrolling/src/App.jsx
+++ infinite-scrolling/src/App.jsx
@@ -2,26 +2,59 @@ import React, { useState, useEffect } from "react";
 import DogCard from "./Components/DogCard";
 function App() {
   const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
   let performApiCall = async () => {
+    setLoading(true)
     const response = await fetch(
-      "https://api.thedogapi.com/v1/images/search?page=1&limit=10"
+      `https://api.thedogapi.com/v1/images/search?page=1&limit=10`
     );
     let data = await response.json();
     console.log(data);
 
-    setData(data);
+    setData((prev) => [...prev, ...data]);
+    setLoading(false)
+  };
+  const handleScrolling = () => {
+    let topHeight = document.documentElement.scrollTop;
+    let innerHeight = window.innerHeight;
+    let scrollHeight = document.documentElement.scrollHeight;
+
+    try {
+      if (topHeight + innerHeight >= scrollHeight) {
+        setPage((prev) => prev + 1);
+      }
+    } catch (error) {
+      return error;
+    }
   };
 
   useEffect(() => {
     performApiCall();
+  }, [page]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrolling);
+    return () => window.removeEventListener("scroll", handleScrolling);
   }, []);
 
   return (
     <>
-      <h1 className="">Infinite Scrolling</h1>
-      {data.map((dog) => (
-        <DogCard dogData={dog} key={dog.id} />
-      ))}
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold text-center mb-6">
+          Infinite Scrolling
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {data.map((dog) => (
+            <DogCard dogData={dog} key={dog.id} />
+          ))}
+        </div>
+        {loading && (
+          <p className="text-center text-lg font-semibold mt-4">
+            Loading more...
+          </p>
+        )}
+      </div>
     </>
   );
 }
--- infinite-scrolling/src/Components/DogCard.jsx
+++ infinite-scrolling/src/Components/DogCard.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const DogCard = ({ dogData }) => {
   return (
-    <div className="card">
-      <p>id : {dogData.id}</p>
-      <img src={dogData.url} width="300px" height="200px" />
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden transition-transform transform hover:scale-105">
+      <img src={dogData.url} alt="Dog" className="w-full h-60 object-cover" />
+      <div className="p-4 text-center">
+        <p className="text-lg font-semibold text-gray-700">🐾 Cute Dog 🐾</p>
+      </div>
     </div>
   );
 };
